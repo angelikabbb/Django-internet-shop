bc3a36ec946208ac1db4126f9f9b9dab525d90e1
angelikabbb <bondarenko.as@edu.spbstu.ru>
2023-09-22 22:01:52 +0300

    add new files

--- cart/forms.py
+++ cart/forms.py
@@ -9,6 +9,17 @@ from django import forms
 # ]
 
 class CartAddProductForm(forms.Form):
+
+    quantity = forms.IntegerField(
+        max_value= 1000,
+        min_value= 1,
+        label='Количество',
+        widget=forms.NumberInput(attrs={'style': 'width:80px', 'id':'quantity', 'onchange':'sendCount(this)'}),
+        initial=1
+        )
+    override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
+
+    
     # quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int, label='Количество')
-    quantity = forms.IntegerField(max_value=1000, label='Количество')
-    override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput )
+    # quantity = forms.IntegerField(max_value=1000, label='Количество')
+    # override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput )
